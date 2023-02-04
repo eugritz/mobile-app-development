de5aa3bcb06ee3f7522f8291375e95d6fc191e74
Eugene Gritz <spicywin@gdlbo.net>
2023-02-04 23:28:40 +0600

    Replace Butterknife with view bindings

--- app/src/main/java/ru/omgtu/lab01_intro/BlankFragment.kt
+++ app/src/main/java/ru/omgtu/lab01_intro/BlankFragment.kt
@@ -5,12 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
-import butterknife.BindView
-import butterknife.ButterKnife
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import ru.omgtu.lab01_intro.databinding.FragmentBlankBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,34 +14,32 @@ import com.google.android.material.textfield.TextInputLayout
  * create an instance of this fragment.
  */
 class BlankFragment : Fragment() {
-    @BindView(R.id.toast_input_layout) lateinit var toastInputLayout: TextInputLayout;
-    @BindView(R.id.toast_text) lateinit var toastText: TextInputEditText;
-    @BindView(R.id.toast_create) lateinit var toastCreateButton: Button;
+    private lateinit var _binding: FragmentBlankBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+    ): View {
+        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ButterKnife.bind(this, view)
 
-        toastCreateButton.setOnClickListener {
-            if (toastText.text!!.length < MINIMUM_TOAST_LENGTH) {
-                toastInputLayout.error = getString(R.string.toast_error, MINIMUM_TOAST_LENGTH)
+        binding.toastCreate.setOnClickListener {
+            if (binding.toastText.text!!.length < MINIMUM_TOAST_LENGTH) {
+                binding.toastInputLayout.error = getString(R.string.toast_error, MINIMUM_TOAST_LENGTH)
             } else {
-                toastInputLayout.error = ""
-                Toast.makeText(activity, toastText.text, Toast.LENGTH_SHORT).show()
+                binding.toastInputLayout.error = ""
+                Toast.makeText(activity, binding.toastText.text, Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     companion object {
-        const val MINIMUM_TOAST_LENGTH = 3;
+        const val MINIMUM_TOAST_LENGTH = 3
 
         /**
          * Use this factory method to create a new instance of
