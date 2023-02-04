20c3aa8fdfef25f1b8a05022f7e2827e79aea283
Eugene Gritz <spicywin@gdlbo.net>
2023-02-04 20:00:46 +0600

    Initial commit

---
+++ app/src/main/java/ru/omgtu/lab01_intro/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.omgtu.lab01_intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
