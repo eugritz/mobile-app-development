26452ae00b13170fa4c92e44e0b8ffa738efdcf6
Eugene Gritz <spicywin@gdlbo.net>
2023-03-19 18:16:28 +0600

    Add MainFragment, MainFragmentCallbacks

--- lab03/src/main/java/ru/omgtu/lab03/MainActivity.kt
+++ lab03/src/main/java/ru/omgtu/lab03/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.omgtu.lab03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainFragment.MainFragmentCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onButton1Pressed() {
+        Toast.makeText(this, "Button 1", Toast.LENGTH_SHORT).show()
+    }
+    override fun onButton2Pressed() {
+        Toast.makeText(this, "Button 2", Toast.LENGTH_SHORT).show()
+    }
+    override fun onButton3Pressed() {
+        Toast.makeText(this, "Button 3", Toast.LENGTH_SHORT).show()
+    }
+    override fun onButton4Pressed() {
+        Toast.makeText(this, "Button 4", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ lab03/src/main/java/ru/omgtu/lab03/MainFragment.kt
@@ -0,0 +1,75 @@
+package ru.omgtu.lab03
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+import butterknife.OnClick
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    private var callbacks: MainFragmentCallbacks? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MainFragmentCallbacks) {
+            callbacks = context
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        ButterKnife.bind(this, view)
+        return view
+    }
+
+    @OnClick(R.id.button1)
+    fun onButton1Pressed() {
+        callbacks?.onButton1Pressed()
+    }
+
+    @OnClick(R.id.button2)
+    fun onButton2Pressed() {
+        callbacks?.onButton2Pressed()
+    }
+
+    @OnClick(R.id.button3)
+    fun onButton3Pressed() {
+        callbacks?.onButton3Pressed()
+    }
+
+    @OnClick(R.id.button4)
+    fun onButton4Pressed() {
+        callbacks?.onButton4Pressed()
+    }
+
+    interface MainFragmentCallbacks {
+        fun onButton1Pressed()
+        fun onButton2Pressed()
+        fun onButton3Pressed()
+        fun onButton4Pressed()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
